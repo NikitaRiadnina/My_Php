18fd1d0e0955f3e727ea75a32658cc4548eca3d5
Nikita <medvedhopod@gmail.com>
2023-05-23 18:42:24 +0300

    my second commit

--- pr2/index.php
+++ pr2/index.php
@@ -1,1 +1,36 @@
 <?php
+// !!!TODO 1: ваш код обробки GET запиту; виконання запиту через cURL у пошукову систему; підготовка даних для малювання
+$target = 'php';
+if (isset($_GET['search'])) { $target = $_GET['search'];}
+$cx = '205c1fcf769bb4263';
+$apiKey = 'AIzaSyBFUHqYSQtGf4OMRj0-bsG8JR85I9kWBLI';
+$url = "https://www.googleapis.com/customsearch/v1?key={$apiKey}&cx={$cx}&q={$target}";
+$curlC = curl_init();
+curl_setopt($curlC, CURLOPT_URL, $url);
+curl_setopt($curlC, CURLOPT_RETURNTRANSFER, true);
+$getJson = curl_exec($curlC);
+curl_close($curlC);
+$transferJson = json_decode($getJson, true)
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h2>My Browser</h2>
+<form method="GET" action="/index.php">
+    <label for="search">Search:</label>
+    <input type="text" id="search" name="search" value=""><br><br>
+    <input type="submit" value="Submit">
+</ form >
+<?php
+// !!! TODO 2: код відображення відповіді
+foreach ($transferJson['items'] as $items) {
+    echo "<a href={$items['link']}>";
+    echo "<p>{$items['title']}</p>";
+}
+?>
+</body>
+</html>
